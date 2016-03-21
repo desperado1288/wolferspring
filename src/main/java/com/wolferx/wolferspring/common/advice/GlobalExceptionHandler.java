a4ed5af4cb5d713655902413fc5da56a7d049eba
丁 亿 <yd2277@columbia.edu>
2016-03-20 20:51:33 -0400

    [service]show bad request exception

--- src/main/java/com/wolferx/wolferspring/common/advice/GlobalExceptionHandler.java
+++ src/main/java/com/wolferx/wolferspring/common/advice/GlobalExceptionHandler.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@ControllerAdvice
+//@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({IllegalArgumentException.class, NullPointerException.class, BaseException.class})
