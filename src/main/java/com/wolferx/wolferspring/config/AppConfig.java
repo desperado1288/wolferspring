035a05ca10c94f35695253b4504bb2f71fc94567
Chen Fang <chenfanggm@hotmail.com>
2016-03-23 22:18:06 -0700

    [merge] solve confict

--- src/main/java/com/wolferx/wolferspring/config/AppConfig.java
+++ src/main/java/com/wolferx/wolferspring/config/AppConfig.java
@@ -2,15 +2,12 @@ package com.wolferx.wolferspring.config;
 
 import com.wolferx.wolferspring.common.filter.CorsFilter;
 import com.wolferx.wolferspring.jdbi.dao.PostDao;
-import org.apache.catalina.filters.CorsFilter;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.skife.jdbi.v2.DBI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import javax.servlet.Filter;
 
 import javax.servlet.Filter;
 
